a395c6cbd0f367834e7e846c3f753d960eaab2ec
ddoniddoni <psdkei@naver.com>
2025-11-04 19:32:43 +0900

    [추가] => Logo 설정

--- app/core/components/@layout/menu/index.tsx
+++ app/core/components/@layout/menu/index.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import Link from 'next/link';
+
 import Navigation from './navigation';
 
 export default function Menu() {
   return (
     <header className="menu">
-      <h2>로고</h2>
+      <Link href="/lobby">
+        <h2 className="service-logo">로고</h2>
+      </Link>
       <Navigation />
     </header>
   );
